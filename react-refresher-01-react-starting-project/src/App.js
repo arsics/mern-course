c57ac670007700c5d635635ad095989bc197f86f
Sasha Arsic <arsic_sasha@yahoo.com>
2019-12-01 16:55:53 +0100

    Section4 Non-functional App component + notes file

--- react-refresher-01-react-starting-project/src/App.js
+++ react-refresher-01-react-starting-project/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const App = () => {
-  return <h1>A React App!</h1>;
-};
+// const App = () => {
+//   return <h1>A React App!</h1>;
+// };
+
+// equivalent, non-function react component:
+class App extends React.Component {
+
+  render() {
+    return <h1>A React App!</h1>;
+  }
+}
 
 export default App;
