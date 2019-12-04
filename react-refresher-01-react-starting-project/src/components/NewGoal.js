6ea9f9b44fa9868878cb57c51bec153734ebe516
Sasha Arsic <arsic_sasha@yahoo.com>
2019-12-04 21:20:42 +0100

    Section4 Component handles user input

--- react-refresher-01-react-starting-project/src/components/NewGoal.js
+++ react-refresher-01-react-starting-project/src/components/NewGoal.js
@@ -3,22 +3,28 @@ import './NewGoal.css';
 
 const NewGoal = props => {
 
+    let enteredText = '';
+
     const addGoalHandler = event  => {
         event.preventDefault(); // don't send the event to a backend
 
         // get the user's input (currently mocked)
         const newGoal = {
             id: Math.random().toString(),
-            text: 'New goal!'
+            text: enteredText
         };
 
         // notify the parent (call his callback function)
         props.onAddGoal(newGoal);
     };
 
+    const textChangeHandler  = event => {
+        enteredText = event.target.value;
+    }
+
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" />
+            <input type="text" onChange={textChangeHandler} />
             <button type="submit">Add goal</button>
         </form>
     );
