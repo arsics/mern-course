7f8ceecc8f743835fb9a7d3e20fc71d81e5c2ffe
Sasha Arsic <arsic_sasha@yahoo.com>
2019-12-04 19:36:59 +0100

    Section4 App with multiple components

--- react-refresher-01-react-starting-project/src/App.js
+++ react-refresher-01-react-starting-project/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
+import GoalList from './components/GoalList';
 
 const App = () => {
+
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn course topic'},
+    {id: 'cg3', text: 'Help other students in Q&A'},
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul className="goal-list">
-        <li>Finish the course</li>
-        <li>Learn course topic</li>
-        <li>Help other students in Q&amp;A</li>
-      </ul>
+      <GoalList goals={courseGoals} />
     </div>
   )
 };
-
 
 export default App;
