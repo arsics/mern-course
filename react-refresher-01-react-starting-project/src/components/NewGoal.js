6e210ee5ca1d0904e4c14ea99857917f407fea47
Sasha Arsic <arsic_sasha@yahoo.com>
2019-12-04 21:43:13 +0100

    Section4 Component handles user input & resets it on submit

--- react-refresher-01-react-starting-project/src/components/NewGoal.js
+++ react-refresher-01-react-starting-project/src/components/NewGoal.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewGoal.css';
 
 const NewGoal = props => {
 
-    let enteredText = '';
+    const [enteredText, setEnteredText] = useState('');
 
     const addGoalHandler = event  => {
         event.preventDefault(); // don't send the event to a backend
@@ -16,15 +16,16 @@ const NewGoal = props => {
 
         // notify the parent (call his callback function)
         props.onAddGoal(newGoal);
+        setEnteredText('');
     };
 
     const textChangeHandler  = event => {
-        enteredText = event.target.value;
+        setEnteredText(event.target.value);
     }
 
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" onChange={textChangeHandler} />
+            <input type="text" value={enteredText} onChange={textChangeHandler} />
             <button type="submit">Add goal</button>
         </form>
     );
